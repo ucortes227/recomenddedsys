b619f2eff5652be11a89f060a46dccf0370770b8
Ulises Cortés San Miguel <ucortes227@gmail.com>
2023-06-26 01:54:36 -0600

    primera version

---
+++ recommendation_rules.pl
@@ -0,0 +1,15 @@
+% Preferencias de usuarios
+user_preference(john, action).
+user_preference(john, adventure).
+user_preference(john, sci-fi).
+user_preference(lisa, drama).
+user_preference(lisa, romance).
+
+% Características de productos
+product_genre(movie1, action).
+product_genre(movie1, sci-fi).
+product_genre(movie2, drama).
+product_genre(movie2, romance).
+product_genre(movie3, action).
+product_genre(movie3, adventure).
+product_genre(movie3, sci-fi).
